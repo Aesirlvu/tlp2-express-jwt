7b007d7783e5c1d0c266d062a824dc8c788b35d1
Ramiro_sanchezzz <ramiroaanchez345@gmail.com>
2024-09-04 12:49:29 -0300

    chore: Update app.js, auth.controller.js

--- backend-session/src/config/cors.js
+++ backend-session/src/config/cors.js
@@ -1,5 +1,4 @@
 export const corsConfig = {
-  // Permitir solicitudes desde el front-end
   origin: ["http://localhost:3000", "http://localhost:3000"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true, // Habilitar envío de cookies
--- backend-session/src/config/session.js
+++ backend-session/src/config/session.js
@@ -1,4 +1,4 @@
-import { env } from "./environment";
+import { env } from "./environment.js";
 
 export const sessionConfig = {
   secret: env.SESSION.SECRET,
